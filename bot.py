21c57f0547a3b0dae043b8a2bba74f0592beab12
Adytzu <40206851+Adytzuma@users.noreply.github.com>
2018-07-25 14:59:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ bot = commands.Bot(command_prefix='d!')
 logging.basicConfig(level='INFO')
 bot.remove_command('help')
 bot.load_extension('admin')
-bot.load_extension('music.py')
+bot.load_extension('music')
 colors = [discord.Colour.purple(), discord.Colour.blue(), discord.Colour.red(), discord.Colour.green(), discord.Colour.orange()]
 admin1 = [404708655578218511]
 admin2 = [390540063609454593]
